ca1314aac4c373214a3679ea31149de40fe66da7
enzo calogero <enzus@hotmail.com>
2020-12-13 13:35:31 -0500

    2020_12_13_ras

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Tensorflow_Lite_embeded
+
+to install interpreter for Raspbery:
+
+pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp37-cp37m-linux_armv7l.whl
--- raspbery/simplified/simplified.py
+++ raspbery/simplified/simplified.py
@@ -1,16 +1,4 @@
-# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 
 from tflite_runtime.interpreter import Interpreter
 from PIL import Image
@@ -27,7 +15,7 @@ args = parser.parse_args()
 filename = args.filename
 model_path = args.model_path 
 
-labels = ['Rock', 'Paper', 'Scissors']
+labels = ['Cat', 'Dog']
 
 # Load TFLite model and allocate tensors
 interpreter = Interpreter(model_path=model_path)
@@ -58,5 +46,7 @@ interpreter.invoke()
 
 # Obtain results and print the predicted category
 predictions = interpreter.get_tensor(output_details[0]['index'])
-predicted_label = np.argmax(predictions)
-print(labels[predicted_label])
+print(predictions)
+
+#predicted_label = np.argmax(predictions)
+#print(labels[predicted_label])
