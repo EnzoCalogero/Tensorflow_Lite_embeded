2b06552a8684817b52f2eebfe07c7ffe6d380182
enzo calogero <enzus@hotmail.com>
2021-04-21 18:08:56 -0400

    22-04-2021

---
+++ raspbery/Enzo_mod_01/classify_picamera_servo.py
@@ -0,0 +1,117 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import io
+import time
+import numpy as np
+import picamera
+
+from time import sleep
+from adafruit_servokit import ServoKit
+
+from PIL import Image
+from tflite_runtime.interpreter import Interpreter
+
+kit = ServoKit(channels=16)
+
+def load_labels(path):
+  with open(path, 'r') as f:
+    return {i: line.strip() for i, line in enumerate(f.readlines())}
+
+
+def set_input_tensor(interpreter, image):
+  tensor_index = interpreter.get_input_details()[0]['index']
+  input_tensor = interpreter.tensor(tensor_index)()[0]
+  input_tensor[:, :] = image
+
+
+def classify_image(interpreter, image, top_k=1):
+  """Returns a sorted array of classification results."""
+  set_input_tensor(interpreter, image)
+  interpreter.invoke()
+  output_details = interpreter.get_output_details()[0]
+  #print(output_details['index'])
+  #print(interpreter.get_tensor(output_details['index']))
+  output = np.squeeze(interpreter.get_tensor(output_details['index']))
+  print(output)
+  # If the model is quantized (uint8 data), then dequantize the results
+  if output_details['dtype'] == np.uint8:
+    scale, zero_point = output_details['quantization']
+    output = scale * (output - zero_point)
+
+  ordered = np.argpartition(-output, top_k)
+  print(ordered[0])
+  return [(i, output[i]) for i in ordered[:top_k]]
+
+def servo_ctrl(id=0):
+    if (id==3):  #nothing
+        kit.servo[0].angle = 0
+        kit.servo[1].angle = 0
+        kit.servo[2].angle = 0
+    elif (id ==2): # closed hand
+        kit.servo[0].angle = 0
+        kit.servo[1].angle = 0
+        kit.servo[2].angle = 90
+    elif (id ==1): #more than one finger
+        kit.servo[0].angle = 0
+        kit.servo[1].angle = 90
+        kit.servo[2].angle = 0
+    elif (id ==0): #one finger
+        kit.servo[0].angle = 90
+        kit.servo[1].angle = 0
+        kit.servo[2].angle = 0    
+    return 0    
+
+def main():
+  
+
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument(
+      '--model', help='File path of .tflite file.', required=True)
+  parser.add_argument(
+      '--labels', help='File path of labels file.', required=True)
+  args = parser.parse_args()
+
+  labels = load_labels(args.labels)
+
+  interpreter = Interpreter(args.model)
+  interpreter.allocate_tensors()
+  _, height, width, _ = interpreter.get_input_details()[0]['shape']
+
+  with picamera.PiCamera(resolution=(640, 480), framerate=30) as camera: # it was 30
+    camera.start_preview()
+    try:
+      stream = io.BytesIO()
+      for _ in camera.capture_continuous(
+          stream, format='jpeg', use_video_port=True):
+        stream.seek(0)
+        
+        img = Image.open(stream).convert('RGB').resize((width, height),
+                                                         Image.ANTIALIAS)
+        img = np.array(img, dtype=np.float32)
+        img = img / 255.
+
+        # Add a batch dimension
+        input_data = np.expand_dims(img, axis=0)
+
+
+        start_time = time.time()
+        results = classify_image(interpreter, img)
+        elapsed_ms = (time.time() - start_time) * 1000
+        label_id, prob = results[0]
+        stream.seek(0)
+        stream.truncate()
+        camera.annotate_text = '%s %.2f\n%.1fms' % (labels[label_id], prob,
+                                                    elapsed_ms)
+        
+        servo_ctrl(id=label_id)
+        #time.sleep(5) #Enzo
+    finally:
+      camera.stop_preview()
+
+
+if __name__ == '__main__':
+  main()
--- raspbery/Enzo_mod_01/servo_tests.py
+++ raspbery/Enzo_mod_01/servo_tests.py
@@ -1,8 +1,41 @@
 from time import sleep
+
+import board
+import busio
+#import adafruit_pca9685
+#i2c = busio.I2C(board.SCL, board.SDA)
+#pca = adafruit_pca9685.PCA9685(i2c)
+
+#led_channel = pca.channels[3]
+#led_channel.duty_cycle = 0
+#sleep(2)
+#led_channel.duty_cycle = 0xffff
+
+
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
-kit.servo[0].angle = 170
-kit.servo[1].angle = 170
-sleep(1)
-kit.servo[0].angle = 10
-kit.servo[1].angle = 10
+#kit.servo[3].angle =170
+#status=0
+for status in {0,1,2,3,4}:
+    if (status==0):
+        kit.servo[0].angle = 0
+        kit.servo[1].angle = 0
+        kit.servo[2].angle = 0
+    elif (status==1):
+        kit.servo[0].angle = 90
+        kit.servo[1].angle = 0
+        kit.servo[2].angle = 0
+    elif (status==2):
+        kit.servo[0].angle = 0
+        kit.servo[1].angle = 90
+        kit.servo[2].angle = 0
+    elif (status==3):
+        kit.servo[0].angle = 0
+        kit.servo[1].angle = 0
+        kit.servo[2].angle = 90
+    elif(status==4):
+        kit.servo[0].angle = 90
+        kit.servo[1].angle = 90
+        kit.servo[2].angle = 90
+    sleep(1)    
+        
