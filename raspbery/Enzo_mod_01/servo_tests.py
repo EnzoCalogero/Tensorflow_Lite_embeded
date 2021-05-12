a8719f26dc7d3f94f70b8c21c4660b81b2c6d9ce
enzo calogero <enzus@hotmail.com>
2021-05-12 17:28:47 -0400

    2021 05 12

--- raspbery/Enzo_mod_01/servo_tests.py
+++ raspbery/Enzo_mod_01/servo_tests.py
@@ -16,26 +16,26 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 #kit.servo[3].angle =170
 #status=0
-for status in {0,1,2,3,4}:
+for status in {0,1,2,3,0}:
     if (status==0):
-        kit.servo[0].angle = 0
-        kit.servo[1].angle = 0
-        kit.servo[2].angle = 0
+        kit.servo[0].angle = 10
+        kit.servo[1].angle = 10
+        kit.servo[2].angle = 10
     elif (status==1):
-        kit.servo[0].angle = 90
-        kit.servo[1].angle = 0
-        kit.servo[2].angle = 0
+        kit.servo[0].angle = 80
+        kit.servo[1].angle = 10
+        kit.servo[2].angle = 10
     elif (status==2):
-        kit.servo[0].angle = 0
-        kit.servo[1].angle = 90
-        kit.servo[2].angle = 0
+        kit.servo[0].angle = 10
+        kit.servo[1].angle = 80
+        kit.servo[2].angle = 10
     elif (status==3):
-        kit.servo[0].angle = 0
-        kit.servo[1].angle = 0
-        kit.servo[2].angle = 90
+        kit.servo[0].angle = 10
+        kit.servo[1].angle = 10
+        kit.servo[2].angle = 70
     elif(status==4):
-        kit.servo[0].angle = 90
-        kit.servo[1].angle = 90
-        kit.servo[2].angle = 90
+        kit.servo[0].angle = 80
+        kit.servo[1].angle = 80
+        kit.servo[2].angle = 70
     sleep(1)    
         
